f348e623e576c05267bd04ade1d2bf3208cc4873
sammu <sammu@SamMulla>
2021-08-05 19:39:48 +0530

    javaScript Add

--- Billing/WebContent/Script.js
+++ Billing/WebContent/Script.js
@@ -139,7 +139,7 @@ window.onload = function () {
             console.log(window);
             var opt = {
                 margin: 1,
-                filename: 'myfile.pdf',
+                filename: 'InvoiceDeom.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
